73714b148d7c063049f998e7d029b7aed73a43a6
mlaopane <mlaopane@gmail.com>
2017-04-19 22:03:40 +0200

    Update the caption event (not tested yet)

--- src/events.js
+++ src/events.js
@@ -1,26 +1,32 @@
 /*** Caption ***/
 
-function initEvents()
-{
-  initCaptions();
-}
-
-function initCaptions()
-{
-  $('.toggle-caption').on('click', toggleContent);
-  function toggleContent(event)
-  {
-    $toggle = $(this);
-    $caption = $($this.closest('.ness-caption'));
-    $title = $($caption.find('.title'));
-    $content = $($caption.find('.content'));
-    $caption.toggleClass('hide');
-  }
-}
-
 var Events = {
-  init: initEvents,
-  initCaptions: initCaptions,
+  duration: 300,
+  /**
+   *
+   */
+  init: function() {
+    this.initCaptions();
+  },
+  /**
+   *
+   */
+  initCaptions: function() {
+    $('.toggle-caption').on('click', toggleContent);
+    /**
+     *
+     * Click Callback
+     */
+    function toggleContent(event) {
+      $toggle = $(this);
+      $icon = $toggle.html();
+      $caption = $($toggle.closest('.ness-caption'));
+      $content = $($caption.find('.content'));
+      $content.slideToggle(this.duration);
+      $icon = $icon == '+' ? '-' : '+';
+      $toggle.html($icon);
+    }
+  }
 }
 
 module.export = Events;
