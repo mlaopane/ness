b299a7d5464415c9da881e5b37ef5f70ed034c2f
mlaopane <mlaopane@gmail.com>
2017-04-26 21:07:31 +0200

    grid : min-width deleted block : margin modified base : titles modified menu : margin modified / effects added

--- src/events.js
+++ src/events.js
@@ -5,16 +5,18 @@ var Events = {
   duration: 300,
   init: function() {
     this.initCaptions();
+    this.initMenus();
   },
   initCaptions: function() {
     /**
      * Click Callback
      */
     let toggleContent = function(event) {
-      $toggle = $(this);
-      $icon = $toggle.html();
-      $caption = $($toggle.closest('.ness-caption'));
-      $content = $($caption.find('.content'));
+      event.preventDefault;
+      let $toggle = $(this);
+      let $icon = $toggle.html();
+      let $caption = $($toggle.closest('.ness-caption'));
+      let $content = $($caption.find('.content'));
       $content.slideToggle(this.duration);
       $icon = $icon == '+' ? '-' : '+';
       $toggle.html($icon);
@@ -22,6 +24,28 @@ var Events = {
     // Event Listener
     $('.toggle-caption').on('click', toggleContent);
   },
+  initCaptions: function() {
+    /**
+     * Click Callback
+     */
+    let toggleContent = function(event) {
+      event.preventDefault;
+      let $this = $(this);
+      let id_menu = $this.data('toggle');
+      let $menu = $($this.parent().find(id_menu));
+      $menu.slideToggle(300, function() { });
+    }
+    // Event Listener
+    $('.toggle-caption').on('click', toggleContent);
+  },
+  /*** Menu ***/
+function toggleMenu(event) {
+  
+}
+
+$(document).ready(function() {
+  $('.toggle-dropdown').on('click', toggleMenu);
+});
 };
 
 module.exports = Events;
