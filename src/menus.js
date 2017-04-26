fdb87e69d330edd7ab7a42e2d156f116444e5f91
mlaopane <mlaopane@gmail.com>
2017-04-27 01:58:40 +0200

    menu.js : Added the burger functionality

--- src/menus.js
+++ src/menus.js
@@ -15,14 +15,13 @@ var Menus = {
         $chevron.toggleClass('fa-chevron-right').toggleClass('fa-chevron-down');
       }
     }
-
     /**
      * 
      * @param {object} event 
      */
-    let toggleMenu = function(event) {
+    let toggleDropdown = function(event, $el = null) {
       event.stopPropagation();
-      let $this = $(this);
+      let $this = $el == null ? $(this) : $el;
       let $menu = $this.closest('.has-dropdown');
       if ( !$menu.length ) {
         return false;
@@ -32,9 +31,32 @@ var Menus = {
       updateChevron($menu, $this);
       $dropdown.slideToggle(this.duration, function() { });
     }
+    /**
+     * 
+     * @param {object} event 
+     */
+    let toggleBurger = function(event) {
+      event.stopPropagation();
+      let $this = $(this);
+      let $burger = $($this.data('toggle'));
+      if ( !$burger.length ) {
+        return false;
+      }
+      dropdowns = $this.closest('.has-burger').find('.dropdown-content');
+      for ( let i = 0 ; i < dropdowns.length ; i++ ) {
+        $dropdown = $(dropdowns[i]);
+        dropdown_toggled = $dropdown.css('display') != 'none' || $dropdown.css('display') == 'block' ;
+        if ( $dropdown.length && dropdown_toggled ) {
+          $toggle_dropdown = $($this.closest('.has-burger').find('.toggle-dropdown'))
+          toggleDropdown(event, $toggle_dropdown);
+        }
+      }
+      $burger.slideToggle(this.duration, function() { });
+    }
 
-    /* Event Listener */
-    $('.toggle-dropdown').on('click', toggleMenu);
+    /* Event Listeners */
+    $('.toggle-dropdown').on('click', toggleDropdown);
+    $('.toggle-burger').on('click', toggleBurger);
 
     /* Toggle Dropdown init */
     let $chevron_right = $('<span>', { class: 'fa fa-chevron-right' });
